db138acec140b5dc453677fc4c964da064e8278d
Unknown <goncalo.almeida88@gmail.com>
2016-12-05 15:05:55 +0000

    AdicionarNovoArtigo added

--- Projecto/Program.cs
+++ Projecto/Program.cs
@@ -10,7 +10,19 @@ namespace Projecto
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Código do artigo:");
+            int codigo = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("Descrição:");
+            string descricao = Console.ReadLine();
+
+            Console.WriteLine("Preço Unitário:");
+            float preco_unitario = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quantidade em stock:");
+            int em_stock = int.Parse(Console.ReadLine());
+
+            Artigo A = new Artigo(codigo, descricao, preco_unitario, em_stock);
         }
     }
 }

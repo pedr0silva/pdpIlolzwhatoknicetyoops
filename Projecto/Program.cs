ccf7f0bc6831aecba806d5581c8bc755eaee5713
Unknown <goncalo.almeida88@gmail.com>
2016-12-05 15:48:22 +0000

    Menu 1.0 Added

--- Projecto/Program.cs
+++ Projecto/Program.cs
@@ -10,6 +10,39 @@ namespace Projecto
     {
         static void Main(string[] args)
         {
+
+            // De Seguida Apresento-vos o "Menu" Para Duros !
+
+            int opcao = 0;
+
+            do
+            {
+                Console.WriteLine("1 - Gestão de Artigos Alimentares");
+                Console.WriteLine("2 - Cartão de Pontos");
+                Console.WriteLine("3 - Guardar Ficheiro (artigos e clientes)");
+                Console.WriteLine("4 - Carregar Ficheiro (artigos e clientes)");
+                Console.WriteLine("0 - Sair");
+
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:                        
+                        break;
+                    case 2:                        
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (opcao != 0);
+
+            // De Seguida Apresento-vos Como "Adicionar Novo Artigo" Para Duros!
+
             Console.WriteLine("Código do artigo:");
             int codigo = int.Parse(Console.ReadLine());
 
@@ -23,6 +56,7 @@ namespace Projecto
             int em_stock = int.Parse(Console.ReadLine());
 
             Artigo A = new Artigo(codigo, descricao, preco_unitario, em_stock);
+
         }
     }
 }

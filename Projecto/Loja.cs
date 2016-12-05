423f47c644e61a06b9f2c7d292f3b91b5b989f99
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2016-12-05 15:19:40 +0000

    Classe Loja Added

---
+++ Projecto/Loja.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projecto
+{
+    public class Loja
+    {
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        private string morada;
+        public string Morada
+        {
+            get { return morada; }
+            set { morada = value; }
+        }
+
+        private int codigo_da_loja;
+        public int Codigo_da_Loja
+        {
+            get { return codigo_da_loja; }
+            set { codigo_da_loja = value; }
+        }
+
+
+        private Dictionary<int,Artigo> dicionario_artigos;
+        public Dictionary<int,Artigo> Dicionario_Artigos
+        {
+            get { return dicionario_artigos; }
+            set { dicionario_artigos = value; }
+        }
+
+        private Dictionary<string,Cliente> dicionario_clientes;
+        public Dictionary<string,Cliente> Dicionario_Clientes
+        {
+            get { return dicionario_clientes; }
+            set { dicionario_clientes = value; }
+        }
+
+        public Loja()
+        {
+
+        }
+    }
+}
